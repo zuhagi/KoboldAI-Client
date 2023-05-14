1e598c9d669fe389914be3d861de7a5056c5afc1
zuhagi <71510475+zuhagi@users.noreply.github.com>
2023-05-14 04:25:56 +0300

    Update colabkobold.sh
    pip

--- colabkobold.sh
+++ colabkobold.sh
@@ -160,6 +160,7 @@ if [ "$init" != "skip" ]; then
     else
         pip install -r requirements.txt
     fi
+    pip uninstall transformers
     pip install git+https://github.com/huggingface/transformers
     # Make sure Colab has the system dependencies
     sudo apt install netbase aria2 -y
